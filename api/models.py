08414e61aedf0c4d4963130f35c6bbdfafc9e9db
i-defranca <ifranceg@gmail.com>
2026-03-30 22:30:37 -0300

    feat: add User model manager, tests and initial migration

--- api/models.py
+++ api/models.py
@@ -1,12 +1,24 @@
 from uuid import uuid4
 
-from django.contrib.auth.models import AbstractBaseUser
-from django.db.models import CharField, UUIDField
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.db import models
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, password=None):
+        if not username:
+            raise ValueError('Username is required!')
+        user = self.model(username=username)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 
 class User(AbstractBaseUser):
-    id = UUIDField(primary_key=True, default=uuid4, editable=False)
-    username = CharField(max_length=150, unique=True)
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    username = models.CharField(max_length=150, unique=True)
+
+    objects = CustomUserManager()
 
     USERNAME_FIELD = 'username'
 
--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,24 @@
+import pytest
+from pytest import mark
+
+from .models import User
+
+
 def test_api_health(client):
     response = client.get('/api/')
     assert response.status_code == 200
+
+
+def create_user(username='username'):
+    return User.objects.create_user(username=username)
+
+
+@mark.django_db
+def test_username_validation():
+    with pytest.raises(ValueError):
+        create_user(None)
+
+
+@mark.django_db
+def test_user_creation():
+    assert str(create_user('username')) == 'username'
