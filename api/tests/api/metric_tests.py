3be3f7f1a3aa2b383b00d26d5b77ea3cad28bf70
i-defranca <ifranceg@gmail.com>
2026-04-01 18:05:31 -0300

    test: update Metric read api endpoints with options and modified data

--- api/tests/api/metric_tests.py
+++ api/tests/api/metric_tests.py
@@ -1,10 +1,10 @@
 from pytest import mark
 
-from api.tests import lorem, new_metric
+from api.tests import lorem, new_metric, new_option, new_user
 
 
 @mark.django_db
-def test_index(client):
+def test_list(client):
     metric = new_metric()
     new_metric()
 
@@ -15,15 +15,20 @@ def test_index(client):
     assert isinstance(data, list)
     assert len(data) == 2
 
-    assert 'id' in data[0]
     assert 'slug' in data[0]
+    assert 'custom' in data[0]
+    assert 'multiple' in data[0]
     assert any(item['slug'] == metric.slug for item in data)
 
 
 @mark.django_db
-def test_get(client):
+def test_retrieve(client):
     new_metric()
+    user = new_user()
     metric = new_metric()
+    new_option(metric, lorem(4))
+    user_option = new_option(metric, lorem(4), user)
+    global_option = new_option(metric, lorem(4))
 
     response = client.get(f'/api/metrics/{metric.slug}/')
     data = response.json()
@@ -31,9 +36,14 @@ def test_get(client):
     assert response.status_code == 200
     assert isinstance(data, dict)
 
-    assert 'id' in data
     assert 'slug' in data
-    assert data['id'] == metric.id
+    assert 'options' in data
+    assert data['slug'] == metric.slug
+
+    assert isinstance(data['options'], list)
+    assert len(data['options']) == 2
+    assert any(item['label'] == global_option.label for item in data['options'])
+    assert not any(item['label'] == user_option.label for item in data['options'])
 
 
 @mark.django_db
